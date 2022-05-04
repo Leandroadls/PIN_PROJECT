7e62d05855b194d2c81929428c785df7eac982e4
Leandro de los Santos <leandrodls@gmail.com>
2022-05-03 22:15:03 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import { BrowserRouter , Routes, Route, Link} from 'react-router-dom';
+import Home from './views/Home';
+import About from './views/About';
+import Services from './views/Services';
+import Products from './views/Products';
+import Contact from './views/Contact';
 import './App.css';
+import 'bootstrap/scss/bootstrap.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter >
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">25Watts</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <Link className="nav-link" to="/">Home</Link>
+              <Link className="nav-link" to="/about">About</Link>
+              <Link className="nav-link" to="/products">Products</Link>
+              <Link className="nav-link" to="/services">Services</Link>
+              <Link className="nav-link" to="/contact">Contact</Link>
+            </div>
+          </div>
+      </div>
+</nav>
+    <div>
+      
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/about" element={<About/>} />
+            <Route path="/products" element={<Products/>} />
+            <Route path="/services" element={<Services/>} />
+            <Route path="/contact" element={<Contact/>} />
+        </Routes>
+        </div>
+      </BrowserRouter>
   );
 }
 
